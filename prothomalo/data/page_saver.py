2cf562e3e5eba8ab76a63e8b29f84f76af946f2b
tanvirfahim15 <tanvircsedu15@gmail.com>
2018-05-03 20:57:47 +0600

    Page saver added

---
+++ prothomalo/data/page_saver.py
@@ -0,0 +1,43 @@
+import requests as req
+import glob
+
+
+def get_article_id(url):
+    url = url[::-1]
+    skip = 0
+    ret = ""
+    for i in url:
+        if i == '/' and skip == 0:
+            skip = 1
+        elif i == '/':
+            skip = 2
+        elif skip == 1:
+            ret = i + ret
+        elif skip == 2:
+            return ret
+
+
+files = []
+
+for file_ in glob.glob("unique*.csv"):
+    files.append(file_)
+    
+print(files)
+
+fileMode = "w"
+directory= "pages/"
+for file_ in files:
+	count = 0
+	article_type = file_.replace('unique_','').replace('.csv','')
+	csv_file = open(file_).readlines()
+	for link in csv_file:
+		count = count + 1
+		resp = req.get(link)
+		page = resp.text
+		article_id = get_article_id(link)
+		out_file = open(directory + article_type + '_' +article_id + "_.txt", fileMode, encoding="utf-8")
+		out_file.write(page)
+		out_file.close()
+		print('File:: '+file_+': '+str(count)+'/'+str(len(csv_file))+' links processed.')
+
+
